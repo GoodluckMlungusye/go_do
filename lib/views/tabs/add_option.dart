ec62087b536cd1c36abc54894b328d347e52350a
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2025-01-10 09:53:46 +0300

    Finalize the conversion

--- lib/views/tabs/add_option.dart
+++ lib/views/tabs/add_option.dart
@@ -1,13 +1,91 @@
+import 'package:go_do/themes/theme_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_do/views/pages/new_category.dart';
+import 'package:go_do/views/pages/new_task.dart';
 
 class AddOptionPage extends StatelessWidget {
   const AddOptionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        elevation: 0,
+        title: const Text(
+          'Select Option',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Text('Option'),
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'You can add new category that matches your task or proceed to create a task based on added categories.',
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 40),
+            _buildOptionButton(
+              context: context,
+              label: "Add Category",
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const NewCategoryPage()),
+                );
+              },
+            ),
+            const SizedBox(height: 30),
+            _buildOptionButton(
+              context: context,
+              label: "Create Task",
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const NewTaskPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOptionButton({
+    required BuildContext context,
+    required String label,
+    required VoidCallback onPressed,
+  }) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * .9,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          side: const BorderSide(width: 1, color: AppColors.primaryColor),
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text(
+            label,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              color: Colors.black45,
+            ),
+          ),
+        ),
       ),
     );
   }
